b03d17f32c78e4e6962634096b0da2e63f288743
emilmalmsten <e.l.malmsten@student.tudelft.nl>
2021-11-26 13:15:38 +0100

    added lesson module with working eureka client on port 8082

--- eureka-client/src/main/java/client/EurekaTestClient.java
+++ eureka-client/src/main/java/client/EurekaTestClient.java
@@ -1,20 +1,18 @@
-package serviceregistrationanddiscoveryclient;
+package client;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ServiceRegistrationAndDiscoveryClientApplication {
+public class EurekaTestClient {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRegistrationAndDiscoveryClientApplication.class, args);
+        SpringApplication.run(EurekaTestClient.class, args);
     }
 }
 
@@ -25,8 +23,8 @@ class ServiceInstanceRestController {
     private DiscoveryClient discoveryClient;
 
     @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
+    public String serviceInstancesByApplicationName(
         @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
+        return "hello world";
     }
 }
--- eureka-service/src/main/java/service/EurekaService.java
+++ eureka-service/src/main/java/service/EurekaService.java
@@ -1,4 +1,4 @@
-package serviceregistrationanddiscoveryservice;
+package service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class ServiceRegistrationAndDiscoveryServiceApplication {
+public class EurekaService {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRegistrationAndDiscoveryServiceApplication.class, args);
+        SpringApplication.run(EurekaService.class, args);
     }
 }
 
