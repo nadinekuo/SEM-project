3324f4a62f838e08705794eadf751d2a106b1c00
emilmalmsten <e.l.malmsten@student.tudelft.nl>
2021-11-26 12:10:19 +0100

    template version with working communication between eureka-service and eureka-client

---
+++ eureka-service/src/main/java/serviceregistrationanddiscoveryservice/ServiceRegistrationAndDiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package serviceregistrationanddiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistrationAndDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistrationAndDiscoveryServiceApplication.class, args);
+    }
+}
+
+//a comment for testing
