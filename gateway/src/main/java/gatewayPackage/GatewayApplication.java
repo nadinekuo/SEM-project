1493844bf0cac82d0f200ccef23d72160ea0246c
Chao Ran Erwin Li <c.r.e.li@student.tudelft.nl>
2021-11-30 18:11:28 +0100

    The gateway application can now route every request to the right service. Added filters that do not filter anything for now, but can be modified later.

--- gateway/src/main/java/gatewayPackage/GatewayApplication.java
+++ gateway/src/main/java/gatewayPackage/GatewayApplication.java
@@ -2,14 +2,23 @@ package gatewayPackage;
 
 import com.netflix.discovery.EurekaClient;
 import gatewayPackage.client.GatewayControllerEureka;
+import gatewayPackage.filters.ErrorFilter;
+import gatewayPackage.filters.Postfilter;
+import gatewayPackage.filters.PreFilter;
+import gatewayPackage.filters.RouteFilter;
 import org.hibernate.validator.internal.util.stereotypes.Lazy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 @RestController
 public class GatewayApplication implements GatewayControllerEureka {
 
@@ -26,7 +35,26 @@ public class GatewayApplication implements GatewayControllerEureka {
 
     @Override
     public String greeting() {
-        return String.format(
-            "Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+        return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
     }
+
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+
+    @Bean
+    public Postfilter postfilter() {
+        return new Postfilter();
+    }
+    //
+    //    @Bean
+    //    public ErrorFilter errorFilter() {
+    //        return new ErrorFilter();
+    //    }
 }
--- reservation/src/main/java/reservationPackage/ReservationApplication.java
+++ reservation/src/main/java/reservationPackage/ReservationApplication.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 import reservationPackage.client.ReservationControllerEureka;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class ReservationApplication implements ReservationControllerEureka {
 
