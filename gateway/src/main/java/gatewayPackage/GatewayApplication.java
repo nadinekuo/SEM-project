ac38081f7a39c4c0a98e59a795d891eaf8dd83b9
Chao Ran Erwin Li <c.r.e.li@student.tudelft.nl>
2021-12-02 19:53:18 +0100

    Added authentication. Still need to add authorization and a connection to the user database.

--- gateway/src/main/java/gatewayPackage/GatewayApplication.java
+++ gateway/src/main/java/gatewayPackage/GatewayApplication.java
@@ -12,13 +12,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
+@EnableEurekaClient
 @RestController
 public class GatewayApplication implements GatewayControllerEureka {
 
@@ -52,9 +53,7 @@ public class GatewayApplication implements GatewayControllerEureka {
     public Postfilter postfilter() {
         return new Postfilter();
     }
-    //
-    //    @Bean
-    //    public ErrorFilter errorFilter() {
-    //        return new ErrorFilter();
-    //    }
+
+    @Bean
+    public ErrorFilter errorFilter() { return new ErrorFilter(); }
 }
