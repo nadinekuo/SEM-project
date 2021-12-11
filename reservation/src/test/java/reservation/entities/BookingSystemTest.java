857319293da47e59aeba65bc52b4b054ca83aa5c
Panagiotis Chatziioannou <P.Chatziioannou-1@student.tudelft.nl>
2021-12-11 17:18:41 +0100

    All tests are passing. Still missing some cases to get 100% line coverage
    Format the test class to avoid code duplication

--- reservation/src/test/java/reservation/entities/BookingSystemTest.java
+++ reservation/src/test/java/reservation/entities/BookingSystemTest.java
@@ -10,10 +10,14 @@ import static org.junit.Assert.assertTrue;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
 import reservation.controllers.ReservationController;
 import reservation.services.ReservationService;
@@ -35,35 +39,22 @@ public class BookingSystemTest {
     List<Reservation> userIdStrategy = new ArrayList<>();
     BookingSystem bookingSystem = new BookingSystem(new ChronologicalStrategy());
 
-    Reservation reservation1 = new Reservation(ReservationType.EQUIPMENT, 10L, 1L,
-        LocalDateTime.of(2020, 1, 1, 1, 1), false);
-    Reservation reservation2 = new Reservation(ReservationType.EQUIPMENT, 3L, 2L,
-        LocalDateTime.of(2020, 1, 1, 1, 1), false);
-    Reservation reservation3 = new Reservation(ReservationType.EQUIPMENT, 5L, 3L,
-        LocalDateTime.of(2020, 4, 1, 1, 1), false);
-    Reservation reservation4 = new Reservation(ReservationType.EQUIPMENT, 20L, 4L,
-        LocalDateTime.of(2020, 3, 1, 1, 1), false);
-    Reservation reservation5 = new Reservation(ReservationType.EQUIPMENT, 4L, 5L,
-        LocalDateTime.of(2020, 5, 1, 1, 1), false);
-    Reservation reservation6 = new Reservation(ReservationType.EQUIPMENT, 5L, 6L,
-        LocalDateTime.of(2020, 6, 1, 1, 1), false);
+
+    Reservation[] reservations;
 
     @BeforeEach
     void setup() {
+        int size = 6;
         restTemplate = Mockito.mock(RestTemplate.class);
-        //Mockito.when(ReservationService.restTemplate()).thenReturn(restTemplate);
-        reservation1.setReservationId(1L);
-        reservation2.setReservationId(2L);
-        reservation3.setReservationId(3L);
-        reservation4.setReservationId(4L);
-        reservation5.setReservationId(5L);
-        reservation6.setReservationId(6L);
-        userIdStrategy.add(reservation1);
-        userIdStrategy.add(reservation2);
-        userIdStrategy.add(reservation3);
-        userIdStrategy.add(reservation4);
-        userIdStrategy.add(reservation5);
-        userIdStrategy.add(reservation6);
+        reservations = new Reservation[size];
+
+        for (int i = 0; i < size; i++) {
+            Reservation reservation = new Reservation(ReservationType.EQUIPMENT, (long) i, (long) i,
+                LocalDateTime.of(2020, i+1, 1, 1, 1), false);
+            reservation.setReservationId(i+1);
+            userIdStrategy.add(reservation);
+            reservations[i] = reservation;
+        }
     }
 
     @Test
@@ -73,21 +64,18 @@ public class BookingSystemTest {
 
     @Test
     void addReservation() {
-        bookingSystem.addReservation(reservation1);
-        assertEquals(reservation1, bookingSystem.getNextReservation());
+        bookingSystem.addReservation(reservations[0]);
+        assertEquals(reservations[0], bookingSystem.getNextReservation());
     }
 
     @Test
     void getNextReservationChronologically() {
         BookingSystem chronologicalStrategy = new BookingSystem(new ChronologicalStrategy());
-        chronologicalStrategy.addReservation(reservation1);
-        chronologicalStrategy.addReservation(reservation2);
-        chronologicalStrategy.addReservation(reservation3);
-        chronologicalStrategy.addReservation(reservation4);
-        chronologicalStrategy.addReservation(reservation5);
-        chronologicalStrategy.addReservation(reservation6);
+        for(Reservation r: reservations){
+            chronologicalStrategy.addReservation(r);
+        }
 
-        assertEquals(reservation6, chronologicalStrategy.getNextReservation());
+        assertEquals(reservations[5], chronologicalStrategy.getNextReservation());
     }
 
     @Test
@@ -95,60 +83,49 @@ public class BookingSystemTest {
         BookingSystem userPremiumStrategy =
             new BookingSystem(new BasicPremiumUserStrategy(restTemplate));
 
-        Mockito.when(restTemplate.getForObject(
-            ReservationController.userUrl + "/user/" + reservation1.getCustomerId()
-                + "/isPremium", Boolean.class)).thenReturn(false);
-        Mockito.when(restTemplate.getForObject(
-            ReservationController.userUrl + "/user/" + reservation2.getCustomerId()
-                + "/isPremium", Boolean.class)).thenReturn(false);
-        Mockito.when(restTemplate.getForObject(
-            ReservationController.userUrl + "/user/" + reservation3.getCustomerId()
-                + "/isPremium", Boolean.class)).thenReturn(false);
-        Mockito.when(restTemplate.getForObject(
-            ReservationController.userUrl + "/user/" + reservation4.getCustomerId()
-                + "/isPremium", Boolean.class)).thenReturn(true);
-        Mockito.when(restTemplate.getForObject(
-            ReservationController.userUrl + "/user/" + reservation5.getCustomerId()
-                + "/isPremium", Boolean.class)).thenReturn(true);
-        Mockito.when(restTemplate.getForObject(
-            ReservationController.userUrl + "/user/" + reservation6.getCustomerId()
-                + "/isPremium", Boolean.class)).thenReturn(true);
+        for (int i = 0; i < 4; i++) {
+            userPremiumStrategy.addReservation(reservations[i]);
 
-        userPremiumStrategy.addReservation(reservation1);
-        userPremiumStrategy.addReservation(reservation2);
-        userPremiumStrategy.addReservation(reservation3);
-        userPremiumStrategy.addReservation(reservation4);
-        userPremiumStrategy.addReservation(reservation5);
-        userPremiumStrategy.addReservation(reservation6);
+            //only third user is premium
+            boolean premium = i==2;
 
-        Reservation first = userPremiumStrategy.getNextReservation();
-        assertTrue(reservation4.equals(first));
+            Mockito.when(restTemplate.getForObject("http://localhost:8084/user/" + reservations[i].getCustomerId() +
+                    "/isPremium",
+                Boolean.class)).thenReturn(premium);
+        }
 
+        assertEquals(reservations[2], userPremiumStrategy.getNextReservation());
     }
 
     @Test
     void getNextReservationEquipmentName() {
         BookingSystem equipmentNameStrategy =
-            new BookingSystem(new EquipmentNameStrategy(ReservationService.restTemplate()));
+            new BookingSystem(new EquipmentNameStrategy(restTemplate));
+
+        for (int i = 0; i < 4; i++) {
+            equipmentNameStrategy.addReservation(reservations[i]);
+        }
 
 
-        equipmentNameStrategy.addReservation(reservation1);
-        equipmentNameStrategy.addReservation(reservation2);
-        equipmentNameStrategy.addReservation(reservation3);
-        equipmentNameStrategy.addReservation(reservation4);
-        equipmentNameStrategy.addReservation(reservation5);
-        equipmentNameStrategy.addReservation(reservation6);
-        assertEquals(userIdStrategy.get(2), equipmentNameStrategy.getNextReservation());
+        String[] names = {"Tango", "Krav Maga", "Ziou Zitsou", "Krav Maga"};
+
+        for (int i = 0; i < 4; i++) {
+            Mockito.when(restTemplate.getForObject(
+                "http://localhost:8085/equipment/" + reservations[i].getSportFacilityReservedId() + "/getEquipmentName",
+                String.class)).thenReturn(names[i]);
+        }
+
+        assertEquals(reservations[1], equipmentNameStrategy.getNextReservation());
     }
 
     @Test
     void getNextReservationUserId() {
         BookingSystem userIdStrategy = new BookingSystem(new UserIdStrategy());
-        userIdStrategy.addReservation(reservation1);
-        userIdStrategy.addReservation(reservation2);
-        userIdStrategy.addReservation(reservation3);
-        userIdStrategy.addReservation(reservation4);
-        assertEquals(reservation2, userIdStrategy.getNextReservation());
+        for (int i = 0; i < 4; i++) {
+            userIdStrategy.addReservation(reservations[i]);
+        }
+
+        assertEquals(reservations[0], userIdStrategy.getNextReservation());
     }
 
     @Test
@@ -159,11 +136,12 @@ public class BookingSystemTest {
 
     @Test
     void toStringTest() {
-        bookingSystem.addReservation(reservation1);
-        bookingSystem.addReservation(reservation2);
-        bookingSystem.addReservation(reservation3);
-        assertEquals("BookingSystem{bookings=[" + reservation1.toString() + ", "
-            + reservation2.toString() + ", " + reservation3.toString() + "]}",
+        for (int i = 0; i < 3; i++) {
+            bookingSystem.addReservation(reservations[i]);
+
+        }
+        assertEquals("BookingSystem{bookings=[" +reservations[0].toString() + ", "
+            + reservations[1].toString() + ", " + reservations[2].toString() + "]}",
             bookingSystem.toString());
     }
 }
