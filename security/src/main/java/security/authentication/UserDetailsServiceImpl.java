a4a3b0320ef440b1220625dd60f3410c1d6e7b60
Emma Wiegers <E.M.J.Wiegers@student.tudelft.nl>
2021-12-16 10:06:31 +0100

    check whether user is a customer or an admin

--- security/src/main/java/security/authentication/UserDetailsServiceImpl.java
+++ security/src/main/java/security/authentication/UserDetailsServiceImpl.java
@@ -1,37 +1,44 @@
-//package securityPackage.authentication;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.AuthorityUtils;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.stereotype.Service;
-//import securityPackage.controllers.AuthenticationController;
-//import security.users.AppUser;
-//
-//@Service   // This is for testing purposes. Will be changed later to get users from database
-//public class UserDetailsServiceImpl implements UserDetailsService {
-//
-//    @Autowired
-//    private BCryptPasswordEncoder encoder;
-//
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//
-//        AppUser user = (AppUser) AuthenticationController.getUserInfo(username);
-//
-//        // If user not found. Throw this exception.
-//        if (user == null) {
-//            throw new UsernameNotFoundException("Username: " + username + " not found");
-//        }
-//
-//
-//        return ;
-//    }
-//
-//
-//}
+package security.authentication;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import security.controllers.AuthenticationController;
+import security.users.AppUser;
+
+@Service   // This is for testing purposes. Will be changed later to get users from database
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        AppUser customer = (AppUser) AuthenticationController.getCustomerInfo(username);
+        AppUser admin = (AppUser) AuthenticationController.getAdminInfo(username);
+
+        // If user not found. Throw this exception.
+        if (customer == null && admin == null) {
+            throw new UsernameNotFoundException("Username: " + username + " not found");
+        }
+
+        if (admin != null) {
+            List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                    .commaSeparatedStringToAuthorityList("ROLE_ADMIN");
+            return new User(admin.getUsername(), admin.getPassword(), grantedAuthorities);
+        }
+
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
+        return new User(customer.getUsername(), customer.getPassword(), grantedAuthorities);
+    }
+
+
+}
