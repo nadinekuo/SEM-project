730a3676459585041233ff817f3c8ada1a7e8a59
Panagiotis Chatziioannou <P.Chatziioannou-1@student.tudelft.nl>
2021-12-17 20:47:27 +0100

    Fixed version of spring boot and checkstyle errors.
    Apparently when I build the project locally it was succesful while it wasn't. Don't know the reason. When I runned a couple of time again it showed the errors

--- user/src/main/java/user/controllers/UserController.java
+++ user/src/main/java/user/controllers/UserController.java
@@ -17,6 +17,9 @@ import user.config.UserDtoConfig;
 import user.entities.Customer;
 import user.services.UserService;
 
+/**
+ * The type User controller.
+ */
 @RestController
 @RequestMapping("user")
 public class UserController {
@@ -27,9 +30,9 @@ public class UserController {
     private final transient RestTemplate restTemplate;
 
     /**
-     * Autowired constructor for the class.
+     * Instantiates a new User controller.
      *
-     * @param userService userService
+     * @param userService the user service
      */
     @Autowired
     public UserController(UserService userService) {
@@ -38,8 +41,10 @@ public class UserController {
     }
 
     /**
-     * @param userId
-     * @return
+     * Is user premium response entity.
+     *
+     * @param userId the user id
+     * @return the response entity
      */
     @GetMapping("/{userId}/isPremium")
     @ResponseBody
@@ -58,8 +63,8 @@ public class UserController {
     /**
      * Customer registration.
      *
-     * @param request the request to register a user.
-     * @throws IOException If customer can't be registered
+     * @param request the request
+     * @throws IOException the io exception
      */
     @PostMapping("/registerCustomer")
     public void customerRegistration(HttpServletRequest request) throws IOException {
@@ -71,8 +76,8 @@ public class UserController {
     /**
      * Admin registration.
      *
-     * @param request the request to register an admin
-     * @throws IOException if admin can't be registered
+     * @param request the request
+     * @throws IOException the io exception
      */
     @PostMapping("/registerAdmin/admin")
     public void adminRegistration(HttpServletRequest request) throws IOException {
