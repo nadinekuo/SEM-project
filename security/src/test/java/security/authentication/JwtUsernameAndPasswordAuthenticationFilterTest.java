84a763128577be9186a84b0209da060b36aac90c
Chao Ran Erwin Li <c.r.e.li@student.tudelft.nl>
2022-01-09 16:52:13 +0100

    JwtTokenAuthenticationFilterTests and UserDetailsServiceImplTest

--- security/src/test/java/security/authentication/JwtUsernameAndPasswordAuthenticationFilterTest.java
+++ security/src/test/java/security/authentication/JwtUsernameAndPasswordAuthenticationFilterTest.java
@@ -1,13 +1,8 @@
 package security.authentication;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
@@ -18,7 +13,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -45,12 +39,12 @@ class JwtUsernameAndPasswordAuthenticationFilterTest {
         JSONObject object = new JSONObject();
         object.put("username", "erwin");
         object.put("password", "randomstring");
-        JwtUsernameAndPasswordAuthenticationFilter filter =
-            new JwtUsernameAndPasswordAuthenticationFilter(authManager, jwtConfig);
         MockHttpServletRequest request = new MockHttpServletRequest("POST", "/eureka-security");
         request.setContentType("application/json");
         request.setContent(object.toString().getBytes(StandardCharsets.UTF_8));
         Authentication mock = mock(Authentication.class);
+        JwtUsernameAndPasswordAuthenticationFilter filter =
+            new JwtUsernameAndPasswordAuthenticationFilter(authManager, jwtConfig);
         when(authManager.authenticate(any())).thenReturn(mock);
         assertNotNull(filter.attemptAuthentication(request, new MockHttpServletResponse()));
     }
