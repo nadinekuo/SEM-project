a92450bb221ef9923c567176f2f901d8eee85d7e
emilmalmsten <e.l.malmsten@student.tudelft.nl>
2022-01-09 16:10:10 +0100

    removed unneccessary javadoc and unused fields, also added "test" after every test method

--- user/src/test/java/user/entities/GroupTest.java
+++ user/src/test/java/user/entities/GroupTest.java
@@ -34,31 +34,31 @@ class GroupTest {
     }
 
     @Test
-    void getGroupId() {
+    void getGroupIdTest() {
         assertThat(group1.getGroupId()).isEqualTo(33L);
         assertThat(group2.getGroupId()).isEqualTo(42L);
     }
 
     @Test
-    void setGroupId() {
+    void setGroupIdTest() {
         group1.setGroupId(56L);
         assertThat(group1.getGroupId()).isEqualTo(56L);
     }
 
     @Test
-    void getGroupName() {
+    void getGroupNameTest() {
         assertThat(group1.getGroupName()).isEqualTo("soccerTeam1");
         assertThat(group2.getGroupName()).isEqualTo("volleyballTeam3");
     }
 
     @Test
-    void setGroupName() {
+    void setGroupNameTest() {
         group1.setGroupName("basketballTeam1");
         assertThat(group1.getGroupName()).isEqualTo("basketballTeam1");
     }
 
     @Test
-    void getGroupMembers() {
+    void getGroupMembersTest() {
         List<Customer> customers = group2.getGroupMembers();
         for (int i = 0; i < customers.size(); i++) {
             if (i == 0) {
@@ -73,7 +73,7 @@ class GroupTest {
     }
 
     @Test
-    void setGroupMembers() {
+    void setGroupMembersTest() {
         List<Customer> newCustomerList = Arrays.asList(arslan, emil, nadine);
 
         group2.setGroupMembers(newCustomerList);
@@ -90,13 +90,13 @@ class GroupTest {
     }
 
     @Test
-    void getGroupSize() {
+    void getGroupSizeTest() {
         assertThat(group1.getGroupSize()).isEqualTo(6L);
         assertThat(group2.getGroupSize()).isEqualTo(3L);
     }
 
     @Test
-    void setGroupSize() {
+    void setGroupSizeTest() {
         group2.setGroupSize(6);
         assertThat(group2.getGroupSize()).isEqualTo(6);
     }
