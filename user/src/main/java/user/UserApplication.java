1c056e1c0f8eb63be020be59ed3bcd1eadb8bcec
emilmalmsten <e.l.malmsten@student.tudelft.nl>
2022-01-09 15:32:44 +0100

    Removed unused files and directories

--- user/src/main/java/user/UserApplication.java
+++ user/src/main/java/user/UserApplication.java
@@ -1,32 +1,15 @@
 package user;
 
-import com.netflix.discovery.EurekaClient;
-import org.hibernate.validator.internal.util.stereotypes.Lazy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-import user.client.UserControllerEureka;
 
 @SpringBootApplication
 @RestController
-public class UserApplication implements UserControllerEureka {
-
-    @Autowired
-    @Lazy
-    private transient EurekaClient eurekaClient;
-
-    @Value("${spring.application.name}")
-    private transient String appName;
+public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
-    }
-
-    @Override
-    public String greeting() {
-        return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
     }
 
 }
